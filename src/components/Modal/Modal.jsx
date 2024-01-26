c5fa00156cf44e8900cfe537353b9d4b32045d65
Heena Nankani <nankaniheena4@gmail.com>
2024-01-27 01:59:52 +0530

    added validation check and formatting

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,13 +3,24 @@ import styles from "./Modal.module.css"
 import { NotesContext } from '../../context/NoteContext'
 
 const Modal = () => {
-    const {isVisible, colors, addNote, closeModal} = useContext(NotesContext);
+    const { isVisible, colors, addNote, closeModal } = useContext(NotesContext);
 
     const [input, setInput] = useState("");
-    const [choosecolor, setChooseColor] =  useState("bg-[#6691FF]")
+    const [choosecolor, setChooseColor] = useState("bg-[#6691FF]")
+    const [error, setError] = useState(false);
 
     const handleChange = (e) => {
         setInput(e.target.value)
+        setError(false)
+    }
+
+    const createGroup = () => {
+        if (input == "") {
+            setError(true);
+        }
+        else {
+            addNote(input, choosecolor);
+        }
     }
 
     if (!isVisible) return null;
@@ -17,24 +28,28 @@ const Modal = () => {
         <div className={styles.background} onClick={closeModal}>
             <div className={styles.modal} onClick={e => e.stopPropagation()}>
                 <div className={styles.heading}>Create New group</div>
-                <div className={styles.inputdiv}>
-                    <div className={styles.name}>Group Name</div>
-                    <input type="text" placeholder='Enter group name' className={styles.input} onChange={(e) => handleChange(e)}/>
+                <div>
+                    <div className={styles.inputdiv}>
+                        <div className={styles.name}>Group Name</div>
+                        <input type="text" placeholder='Enter group name' className={styles.input} onChange={(e) => handleChange(e)} />
+                    </div>
+                    {error ? <div className={styles.error}>Group Name cannot be empty</div> : ""}
                 </div>
+
                 <div className={styles.colordiv}>
                     <div className={styles.name}>Choose colour</div>
                     <div className={styles.choosecolor}>
-                        {colors.map((color)=>{
-                            return(
-                                <div className={`${choosecolor==color? "sm:size-9 size-7" : ""} ${styles.colors} ${color}`} key={color} onClick={() => setChooseColor(color)}></div>
+                        {colors.map((color) => {
+                            return (
+                                <div className={`${choosecolor == color ? "sm:size-9 size-7" : ""} ${styles.colors} ${color}`} key={color} onClick={() => setChooseColor(color)}></div>
                             )
                         })}
                     </div>
                 </div>
                 <div className={styles.button}>
-                <button className={styles.btn} onClick={() => addNote(input, choosecolor)}>Create</button>
+                    <button className={styles.btn} onClick={createGroup}>Create</button>
                 </div>
-                
+
             </div>
         </div>
     )
